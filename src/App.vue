e32ff9923f2bf962b1e5596eaeaa131c98fdbf2f
ash.fayzrahmanov <ash.fayzrahmanov@tensor.ru>
2017-12-12 17:18:43 +0300

    - json добывается через промисы - первый компонент получил свои данные

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,103 @@
 <template>
   <div id="app">
-    <RepairMD/>
+    <RepairMD
+      v-if="repairSJON"
+      :title = "repairSJON.title"
+      :teaser_card = "repairSJON.teaser_card"
+      :children="repairSJON.children"
+    />
+    <RepairData/>
+<!--!!! {{ repairSJON }}  json получен  !!!-->
+
   </div>
-
-
 </template>
 
 <script>
   import RepairMD from './components/RepairMD'
+  import RepairData from './datas/RepairData'
   export default {
     name: 'app',
     components: {
-      RepairMD
+      RepairMD,
+      RepairData
+    },
+    asyncComputed: {
+      repairSJON(){
+        return new Promise((resolve, reject) => {
+          // получаем через промисы JSON
+          setTimeout(function () {
+            console.log(" ==получил JSON= " )
+            resolve(RepairData.dataJson)
+          }, 200)
+        })
+      }
     }
   }
 </script>
 
 <style lang="scss">
-  /*@import '../node_modules/simple-line-icons/css/simple-line-icons.css'*/
-
   $font_path: '~@/assets/fonts/';
+  $dark_blue_color: #476878;
+  $default_text_color: #2f444e;
 
   @font-face {
     font-family: 'DINPro-Regular';
     src: url($font_path + 'DINPro-Regular.eot?#iefix') format('embedded-opentype'),
     url($font_path + 'DINPro-Regular.woff') format('woff'),
-    url($font_path + 'DINPro-Regular.ttf')  format('truetype'),
+    url($font_path + 'DINPro-Regular.ttf') format('truetype'),
     url($font_path + 'DINPro-Regular.svg#Gotham-Light') format('svg');
     font-weight: normal;
     font-style: normal;
     line-height: 1.5;
   }
 
-
-  body{
-    margin:0;
-    padding:0;
+  body {
+    margin: 0;
+    padding: 0;
   }
+
   #app {
-    font-family: "DINPro-Regular";
-    /*font-family: 'DINPro-Regular', Helvetica, Arial, sans-serif;*/
-    color: #2c3e50;
-
+    font-family: 'DINPro-Regular', Helvetica, Arial, sans-serif;
+    color: $default_text_color;
   }
 
+  //<editor-fold desc="Заголовок" defaultstate="collapsed">
   .repair_header {
-    /*font-family: "DINPro-Regular";*/
-    font-family: 'DINPro-Regular', Helvetica, Arial, sans-serif;
-    background: #476878;
-    height: 82px;
+    background: $dark_blue_color;
     font-size: 18px;
     text-align: right;
-    padding-right: 15px;
-    padding-top: 40px;
+    padding: 27px 0;
     color: #fff;
   }
-  .repair_header .icons{
+
+  .repair_header .icons {
     vertical-align: middle;
     font-size: 24px;
+    cursor: pointer;
   }
+
+  .repair_header .icon-settings {
+    padding-left: 10px
+  }
+
+  .repair_header .icon-logout {
+    padding-left: 30px;
+    padding-right: 15px;
+  }
+
+  //</editor-fold>
+  //<editor-fold desc="Контент" defaultstate="collapsed">
+  .repair_content {
+    margin: 0 auto;
+    width: 454px;
+    padding: 50px 0;
+  }
+
+  .repair_content hr {
+    background: rgba(89, 106, 114, 0.2);
+    height: 1px;
+    border: 0;
+  }
+
+  //</editor-fold>
 </style>
--- src/components/RepairMD.vue
+++ src/components/RepairMD.vue
@@ -1,16 +1,40 @@
 <template>
   <div>
 
-  <div class="repair_header">
-    Константинопольский
-    <span class="icon-logout icons"></span>
-</div>
-  <div>RepairMD</div>
+    <div class="repair_header">
+      Константинопольский К.
+      <span class="icon-settings icons"></span>
+      <span class="icon-logout icons"></span>
+    </div>
+    <div class="repair_content">
+
+      <hr noshade/>
+      {{title}}
+      {{teaser_card}}
+
+      <div v-for="child in children">{{child.title}}</div>
+
+
+
+    </div>
   </div>
 </template>
 <script>
+
   export default{
     name: 'RepairMD',
-    props: {}
+    components: {},
+    props: {
+      title: {
+        type: String,
+        require: true
+      },
+      teaser_card:{
+         type:String
+      },
+      children:{
+        type:Array
+      },
+    }
   }
 </script>
