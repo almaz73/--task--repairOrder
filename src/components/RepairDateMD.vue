9dc37dee8633d938fa774e7528a9cfba39a5bde8
ash.fayzrahmanov <ash.fayzrahmanov@tensor.ru>
2017-12-14 11:54:50 +0300

    - показ количество услуг, с правильный склоненем слов - отображение общей суммы

--- src/components/RepairDateMD.vue
+++ src/components/RepairDateMD.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
     <div class="repair_content">
-      <div class="title_head">{{title}}</div>
+      <div class="title_head">{{title}}, {{positions}} {{wordSelection}}</div>
       <span class="summa_text">
         {{totalAmmount}}
         <span>&#8381</span>
       </span>
       Выберите удобный день и время
-
     </div>
   </div>
 </template>
@@ -17,11 +16,26 @@
     data(){
       return {
         title: 'Ремонт',
-        totalAmmount: 222
+        totalAmmount: null,
+        positions: null,
+        wordSelection: "услуг"
       }
     },
     created: function () {
       let orderArr = this.$parent.orderArr
+      let positions = 0
+      orderArr.forEach(el => {
+        this.totalAmmount += el.sum
+        this.positions++
+      })
+      switch (this.positions.toString().slice(-1)) {
+        case "1":
+          this.wordSelection = "услуга";
+          break;
+        case "2":
+          this.wordSelection = "услуги";
+          break;
+      }
     }
   }
 </script>
